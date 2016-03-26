77a0e960089aa1e084124b408b02e76ddde202f7
Ervin <ervinbeciragic@gmail.com>
2016-03-26 18:59:06 +0100

    Reconfigured

--- registration.php
+++ registration.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of Glugox.
+ *
+ * (c) Glugox <glugox@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Glugox_Core',
