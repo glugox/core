201e56fb80413b0c8e74e74d63a963ada861efd6
Glugox <glugox@gmail.com>
2016-05-27 12:09:53 +0200

    Decoupling by moving ProcessServiceInterface to core

---
+++ Api/ProcessServiceInterface.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file is part of Glugox.
+ *
+ * (c) Glugox <glugox@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Glugox\Process\Api;
+
+
+use Magento\Framework\Model\AbstractModel;
+
+interface ProcessServiceInterface
+{
+
+    /**
+     * @param $code
+     * @param array $data
+     */
+    public function getProcess($code, array $data);
+
+
+    /**
+     * @param Instance $process
+     * @return Instance
+     */
+    public function startProcess( AbstractModel $process );
+
+    /**
+     * @param Instance $process
+     * @return Instance
+     */
+    public function finishProcess( AbstractModel $process, $error = null );
+
+
+    /**
+     * @param Instance $process
+     * @return Instance
+     */
+    public function updateProcess( AbstractModel $process, $data= [] );
+}
